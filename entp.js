2f8d39f3bafba2c809e44c35c90d466cbcb3ee34
Siren <143248933+sirentusk@users.noreply.github.com>
2023-11-16 16:12:17 +1100

    Add files via upload

--- entp.js
+++ entp.js
@@ -1,20 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Add click event listeners to all elements with the class 'polygon'
+    document.querySelectorAll('.gallery .polygon').forEach(function(polygon) {
+        polygon.addEventListener('click', toggleExpand);
+    });
+
+    // Add click event listeners to all img elements inside the class 'thumbnail'
+    document.querySelectorAll('.thumbnail img').forEach(function(img) {
+        img.addEventListener('click', toggleExpand);
+    });
+});
+
 function toggleExpand(event) {
     event.preventDefault();
-    var image = event.currentTarget; // The clicked image
+    var image = event.currentTarget; // The clicked element
 
-    // Check if the clicked image already has the 'big' class
+    // Check if the clicked element already has the 'big' class
     if (image.classList.contains('big')) {
         // If it does, remove it and reset the style
         image.classList.remove('big');
         image.style.zIndex = '';
     } else {
-        // If it doesn't, first remove the 'big' class from any other image with 'big'
+        // If it doesn't, first remove the 'big' class from any other element with 'big'
         document.querySelectorAll('.big').forEach(function(el) {
-            el.classList.remove('big'); // Remove 'big' class from all images
+            el.classList.remove('big'); // Remove 'big' class from all elements
             el.style.zIndex = ''; // Reset z-index
         });
 
-        // Then, add the 'big' class to the clicked image
+        // Then, add the 'big' class to the clicked element
         image.classList.add('big');
         image.style.zIndex = 10;
     }
